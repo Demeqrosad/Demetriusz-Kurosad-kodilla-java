c03eba2f852cee4e2a5cc2d2831b7b8e1b677de0
Demeqrosad <demetriusz@kurosad.pl>
2019-10-10 22:38:06 +0200

    6.2 Module

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,29 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args)
     {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        int a = 7;
+        int b = 5;
+//Test of method Calculator.add
+        if (Calculator.add(a,b) == a+b)
+        {
+            System.out.println("Test of method Calculator.add - OK");
+        }
+        else
+        {
+            System.out.println("Error in test of method Calculator.add");
+        }
+//Test of method Calculator.subtract
+        if (Calculator.subtract(a,b) == a-b)
+        {
+            System.out.println("Test of method Calculator.subtract - OK");
+        }
+        else
+        {
+            System.out.println("Error in test of method Calculator.subtract");
+        }
     }
 }
