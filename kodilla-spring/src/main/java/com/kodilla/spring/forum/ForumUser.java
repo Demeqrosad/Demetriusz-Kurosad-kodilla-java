9daf3c85eaa09033c556d4c9cf17e26be64a1ed3
Demeqrosad <demetriusz@kurosad.pl>
2019-11-11 15:56:55 +0100

    Module 10.1 20191111

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,14 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public final class ForumUser
+{
+    private final String username = "John Smith";
+
+    public String getUsername()
+    {
+        return username;
+    }
+}
