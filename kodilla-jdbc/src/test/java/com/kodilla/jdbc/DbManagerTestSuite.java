634cd4e393706b4adcc9a347aa564cc727ce5af7
Demeqrosad <demetriusz@kurosad.pl>
2019-11-25 18:56:47 +0100

    Module 12.6 20191125

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -18,8 +18,10 @@ public class DbManagerTestSuite
         //Then
         Assert.assertNotNull(dbManager.getConnection());
     }
+
     @Test
-    public void testSelectUsers() throws SQLException {
+    public void testSelectUsers() throws SQLException
+    {
         //Given
         DbManager dbManager = DbManager.getInstance();
 
@@ -30,7 +32,8 @@ public class DbManagerTestSuite
 
         //Then
         int counter = 0;
-        while(rs.next()) {
+        while (rs.next())
+        {
             System.out.println(rs.getInt("UserID") + ", " +
                     rs.getString("firstName") + ", " +
                     rs.getString("lastName"));
@@ -39,5 +42,32 @@ public class DbManagerTestSuite
         rs.close();
         statement.close();
         Assert.assertEquals(5, counter);
+    }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException
+    {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        int expResult = 1;
+        //When
+        String sqlQuery = "SELECT CONCAT(U.firstName, \" \", U.lastName) AS user, COUNT(P.postID) AS numberOfPosts\n" +
+                "FROM Users U INNER JOIN Posts P ON U.userID = P.userID\n" +
+                "GROUP BY U.userID\n" +
+                "HAVING COUNT(P.postID) > 1\n" +
+                "ORDER BY U.lastName, U.firstName;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+        //Then
+        int actResult = 0;
+        while (rs.next())
+        {
+            System.out.println(rs.getString("user") + ", " +
+                    rs.getInt("numberOfPosts"));
+            actResult++;
+        }
+        rs.close();
+        statement.close();
+        Assert.assertEquals(expResult, actResult);
     }
 }
